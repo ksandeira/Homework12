176bf829bf48bbf3002faa9f483ffcf260b2b5db
ksandeira <ostapksenia368@gmail.com>
2025-08-13 15:55:25 +0300

    finished commit

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,27 @@
 public class Book {
+   private Author author;
+    private String titleBook;
+    private int publicationYear;
+
+    public Book(Author author, String titleBook, int publicationYear) {
+        this.author = author;
+        this.titleBook = titleBook;
+        this.publicationYear = publicationYear;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public String getTitleBook() {
+        return titleBook;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        Author author1 = new Author("Данте", " Агильери");
+        Author author2 = new Author("Уилки", " Коллинз");
 
+        Book book1 = new Book(author1, "Божественная комедия", 1308);
+        Book book2 = new Book(author2, "Лунный камень", 1868);
+
+        book1.setPublicationYear(1321);
+
+        System.out.println(" Книга 1: " + book1.getTitleBook() + " , автор: " + book1.getAuthor().getFirstName() + book1.getAuthor().getLastName() + " , год публикации: " + book1.getPublicationYear());
+        System.out.println(" Книга 2: " + book2.getTitleBook() + " , автор: " + book2.getAuthor().getFirstName() + book2.getAuthor().getLastName() + " , год публикации: " + book2.getPublicationYear());
         }
     }
